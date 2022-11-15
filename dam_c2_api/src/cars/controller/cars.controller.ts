a995f2d4195d41727f0dd8c11c2106455c15f4dc
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-11-15 10:38:31 -0300

    feat: add controller

--- dam_c2_api/src/cars/controller/cars.controller.ts
+++ dam_c2_api/src/cars/controller/cars.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  HttpStatus,
+} from '@nestjs/common';
+
+import { CarsService } from '../provider/cars.service';
+import { CarsDTO } from '../dtos/cars.dto';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  constructor(private carsService: CarsService) {}
+
+  @Get()
+  async getAll() {
+    return {
+      statusCode: HttpStatus.OK,
+      data: await this.carsService.getAll(),
+    };
+  }
+
+  @Get(':id')
+  async getOne(@Param('id') id: number) {
+    return {
+      statusCode: HttpStatus.OK,
+      data: await this.carsService.getOne(id),
+    };
+  }
+
+  @Post()
+  async create(@Body() data: CarsDTO) {
+    return {
+      statusCode: HttpStatus.OK,
+      data: await this.carsService.create(data),
+    };
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() data: Partial<CarsDTO>) {
+    return {
+      statusCode: HttpStatus.OK,
+      data: await this.carsService.update(id, data),
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return {
+      statusCode: HttpStatus.OK,
+      data: await this.carsService.delete(id),
+    };
+  }
+}
