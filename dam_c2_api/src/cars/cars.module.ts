323c7909fafb9b0b3086a175d6023871bcc0ce5c
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-11-15 10:51:34 -0300

    feat: add db implementation for cars

--- dam_c2_api/src/cars/cars.module.ts
+++ dam_c2_api/src/cars/cars.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarsController } from './controller/cars.controller';
+import { CarsEntity } from './entity/cars.entity';
 import { CarsService } from './provider/cars.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CarsEntity])],
   controllers: [CarsController],
   providers: [CarsService],
 })
