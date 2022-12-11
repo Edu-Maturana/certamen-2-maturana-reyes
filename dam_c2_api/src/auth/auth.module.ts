5068c0b1988691296a3baed804cc34eeabbe0d3e
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-12-11 02:19:41 -0300

    feat: add login handler

--- dam_c2_api/src/auth/auth.module.ts
+++ dam_c2_api/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './provider/auth.service';
 import { AuthConfig } from './auth.config';
+import { AuthController } from './controller/auth.controller';
 
 @Module({
   providers: [AuthService, AuthConfig],
+  controllers: [AuthController],
 })
 export class AuthModule {}
