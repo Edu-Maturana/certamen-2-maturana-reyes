1a2d7dab499d61c97ecd4a66019a94ac00d36149
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-12-11 02:18:06 -0300

    feat: add auth service

--- dam_c2_api/src/auth/auth.module.ts
+++ dam_c2_api/src/auth/auth.module.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { AuthService } from './provider/auth.service';
+import { AuthConfig } from './auth.config';
 
-@Injectable()
-export class AuthConfig {
-  public userPoolId: string = process.env.COGNITO_USER_POOL_ID;
-  public clientId: string = process.env.COGNITO_CLIENT_ID;
-  public region: string = process.env.COGNITO_REGION;
-  public authority = `https://cognito-idp.${process.env.COGNITO_REGION}.amazonaws.com/${process.env.COGNITO_USER_POOL_ID}`;
-}
+@Module({
+  providers: [AuthService, AuthConfig],
+})
+export class AuthModule {}
